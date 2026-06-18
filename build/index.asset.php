1c3c1d5680790ae6e58f43db4fe25742223a5590
shihab-shamim <shihabshamim767@gmail.com.com>
2026-06-18 16:43:28 +0600

    block json update

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => '817956e79d0c3a578f08');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => 'c9f8a6ff6b74f1d38ac9');
